1e1786f9f9767260259361f819277ecf2dc7577a
sina <sina99moradi@gmail.com>
2024-10-28 12:40:17 +0330

    error handling ( 404, 500 )

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 from app.users.routes import blueprint as user_blueprint
 from app.posts.routes import blueprint as post_blueprint
+import app.exceptions as error_handler
 
 
 def register_blueprint(app):
@@ -10,8 +11,14 @@ def register_blueprint(app):
     app.register_blueprint(post_blueprint)
 
 
+def register_error_handlers(app):
+    app.register_error_handler(404, error_handler.page_not_found)
+    app.register_error_handler(500, error_handler.server_error)
+
+
 app = Flask(__name__)
 register_blueprint(app)
+register_error_handlers(app)
 app.config.from_object('config.DevConfig')
 
 db = SQLAlchemy(app)
