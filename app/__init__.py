0ffcf3afbd2d2868b69eda207df2b11c10042dd3
sina <sina99moradi@gmail.com>
2024-10-29 11:29:46 +0330

    base model

--- app/__init__.py
+++ app/__init__.py
@@ -22,5 +22,8 @@ register_error_handlers(app)
 app.config.from_object('config.DevConfig')
 
 db = SQLAlchemy(app)
+
+from app.users.models import User # is here due to circular_imports for db.create_all() use
+
 with app.app_context():
     db.create_all()
